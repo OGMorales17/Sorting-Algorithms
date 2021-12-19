7118af2a11f59c731cab3d31261439333f60fd5f
OGMorales17 <chorly786@gmail.com>
2021-12-19 14:20:23 -0500

    .

--- bubble.js
+++ bubble.js
@@ -1,3 +1,35 @@
-function bubbleSort() {}
+/**
+Implement a function called bubbleSort. Given an array, bubbleSort will sort the values in the array.
+Bubble sort is an O(n^2) algorithm. You can learn about Bubble Sort
+ */
 
-module.exports = bubbleSort;
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1)
+            }
+        }
+    }
+    return arr;
+}
+
+
+bubbleSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
+bubbleSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
+bubbleSort([1, 2, 3]); // [1, 2, 3]
+bubbleSort([]);
+
+let nums = [
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34,
+    23, 2, 453, 546, 75, 67, 4342, 32
+];
+
+bubbleSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67,
+// 75, 232, 232, 453, 546, 4342]
+
+module.exports = { bubbleSort };
